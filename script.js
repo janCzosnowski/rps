a5b8425b3a261c58d63f8778f4ca8ac7bbf61182
janCzosnowski <janczosnowski77@gmail.com>
2022-08-17 21:40:58 +0200

    added player input and a round

--- script.js
+++ script.js
@@ -8,7 +8,46 @@ function getComputerChoice(){
     } else{
         choice = "paper";
     }
+    
+    return choice;
+}
+
+function playerSelection(){
+    let choice = prompt("input rock, paper or scissors: ");
+    
+    choice = choice.toLowerCase();
+
+    while(choice != "rock" && choice != "paper" && choice != "scissors"){
+        choice = prompt("input a valid option: rock, paper or scissors");
+    }
 
     return choice;
 }
-console.log(getComputerChoice());
+
+function round(player, computer){
+    if(player === computer){
+        return "It's a draw!";
+    } else if(player == "rock"){
+        if(computer == "paper"){
+            return "You lost, paper beats rock."
+        }else{
+            return "You won, rock beats scissor!"
+        }
+
+    } else if(player == "paper"){
+        if(computer == "scissors"){
+            return "You lost, scissors beat paper."
+        }else{
+            return "You won, paper beats rock!"
+        }
+    } else if(player == "scissors"){
+        if(computer == "rock"){
+            return "You lost, paper beats scissors."
+        }else{
+            return "You won, scissors beat paper!"
+        }
+
+    }
+}
+
+console.log(round(playerSelection(), getComputerChoice()));
